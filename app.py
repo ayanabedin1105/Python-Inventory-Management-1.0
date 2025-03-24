496b2403af605383a22b48b2f4b572c6a04c1bb2
ayanabedin1105 <ayanabedin@gmail.com>
2025-03-24 11:05:00 +0000

    logins and authentication created

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import sqlite3
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 # define app name
 app = Flask(__name__)
+app.secret_key = '1105hotmail'
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 # create datasbase and table if doesn't exists
 def init_db():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users 
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL)''')
+
     c.execute('''CREATE TABLE IF NOT EXISTS inventory
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
                  name TEXT NOT NULL,
@@ -16,6 +26,16 @@ def init_db():
     conn.close()
 
 init_db()
+
+# User class
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
 
 @app.route('/')
 def index():
@@ -35,16 +55,30 @@ def add_item():
     c.execute("INSERT INTO inventory (name, quantity) VALUES (?,?)", (name, quantity))
     conn.commit()
     conn.close()
-    return redirect('/')
+    return jsonify({"name": name, "quantity": quantity})
 
-@app.route('/delete/<int:item_id>')
+@app.route('/delete/<int:item_id>', methods=["POST"])
+@login_required
 def delete_item(item_id):
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("DELETE FROM inventory WHERE id=?", (item_id,))
     conn.commit()
     conn.close()
-    return redirect('/')
+    return jsonify({"status": "success", "item_id": item_id})
+
+@app.route('/update/<int:item_id>', methods=['POST'])
+@login_required
+def update_item(item_id):
+    item_id = request.form["id"]
+    quantity = request.form["quantity"]
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("UPDATE inventory SET quantity=? WHERE id=?", (quantity, item_id))
+    conn.commit()
+    conn.close()
+    
+    return jsonify({"status": "success", "item_id": item_id, "quantity": quantity})
 
 if __name__=="__main__":
     app.run(debug=True)
